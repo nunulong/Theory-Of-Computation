d93a407a036e2c0b58a6d566b599384776e6d736
Ting Wang <felix.wang.1026@gmail.com>
2018-03-20 23:53:16 -0400

    finished the regex

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -3,18 +3,20 @@ const fs = require('fs');
 const args = process.argv.slice(2);
 
 if (args.length != 1) {
-    console.error("usage: extractlinks inputfile");
+    console.error('usage: extractlinks inputfile');
     process.exit(1);
 }
 
 const filename = args[0];
 
-// !!!! IMPLEMENT ME
-
 // Read file
+const data = fs.readFileSync(filename, { encoding: 'utf8' });
 
 // Set up regex
+const reg = /https?:\/\/[^\\'">\s]+?\.[^\\'">\s]+/g;
 
 // Find matches
+const match = data.match(reg);
 
 // Print all matches
+console.log(match);
--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -1,23 +1,23 @@
 var readline = require('readline');
 
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
 });
 
+// Come up with the phone regex
+const reg = /(\d)+(\)?)(^-|^\s)?(\d)/g;
 
 // This code reads a line at a time from stdin
-
-rl.on('line', function (line) {
-
-    // !!!! IMPLEMENT ME
-
-    // Come up with the phone regex
-
+rl.on('line', function(line) {
     // Find matches
-
+    const data = line.match(reg);
     // If match found, print number with no spaces, parens, or dashes
-
+    if (data) {
+        console.log(data.join(''));
+    } else {
+        console.log('No Number Found');
+    }
     // Else print that no number was found
 });
